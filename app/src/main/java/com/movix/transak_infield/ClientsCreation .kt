91245241f30ea6cc3818ff310b808f7be73a4a77
Megistanas <nelvinkelly@gmail.com>
2025-07-31 01:42:39 +0300

    third amendment

--- app/src/main/java/com/movix/transak_infield/ClientsCreation .kt
+++ app/src/main/java/com/movix/transak_infield/ClientsCreation .kt
@@ -3,5 +3,5 @@ package com.movix.transak_infield
 import java.sql.Date
 
 open class ClientsCreation (val id:Int,val name:String,val phone:String,vararg creationDate:Date) {}
-class Estimateinfo(val id:Int,var creationDate:String,var dueDate: String)
+class Estimateinfo(val id:Int,var creationDate:String,var dueDate: String,val customerId:Int)
 
